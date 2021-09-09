977777c58abff25772ffcd4c9b80381611afbde3
Noa <33094578+coolreader18@users.noreply.github.com>
2021-09-08 20:14:00 -0500

    Move all python stuff to subdir

--- api/make/index.py
+++ api/make/index.py
@@ -1,21 +1,23 @@
-import buttonsheet
+from . import buttonsheet
 from flask import Flask, request
 from PIL import Image
 import urllib.parse
 
 app = Flask("sheetmaker")
 
+
 @app.route("/<path:path>", methods=["POST"])
 def make(path):
-    f = request.files['image']
+    f = request.files["image"]
     im = Image.open(f)
     pdf = buttonsheet.make(im)
     part = f.filename.rpartition(".")
     fname = (part[0] or part[2]) + ".pdf"
     return pdf.output(), {
         "Content-Type": "application/pdf",
-        "Content-Disposition": f"attachment; filename*={rfc5987(fname)}"
+        "Content-Disposition": f"attachment; filename*={rfc5987(fname)}",
     }
+
 
 def rfc5987(s):
     return "UTF-8''" + urllib.parse.quote(s, encoding="utf-8")
