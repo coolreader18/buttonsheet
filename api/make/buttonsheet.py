f115690b82a8b0539f44f0514095c3104db6d364
Noa <33094578+coolreader18@users.noreply.github.com>
2021-09-08 23:04:53 -0500

    Finish mvp-2

--- api/make/buttonsheet.py
+++ api/make/buttonsheet.py
@@ -1,5 +1,7 @@
+from typing import Iterable, Tuple
 from PIL import Image
 from fpdf import FPDF
+import itertools
 
 __all__ = ["make"]
 
@@ -8,6 +10,8 @@ outer_button = 1.625
 page_width = 8.5
 page_height = 11
 
+outer_radius = (outer_button / 2) - (inner_button / 2)
+
 
 def inch_to_ps(*x):
     return tuple(p * 72 for p in x)
@@ -15,15 +19,20 @@ def inch_to_ps(*x):
 
 def make_poss(n, w, padding, startp):
     p = startp
-    for nrow in range(n):
+    for _nrow in range(n):
         yield p
         p += w + padding
 
 
+def calc_dims(x, y):
+    inner_dims = x + outer_radius, y + outer_radius, inner_button, inner_button
+    outer_dims = x, y, outer_button, outer_button
+    return inner_dims, outer_dims
+
+
 poss = lambda n: make_poss(n, outer_button, 0.5, 0.25)
 
-button_positions = [(x, y) for x in poss(4)
-                    for y in poss(5)]
+button_positions = [(x, y, *calc_dims(x, y)) for x in poss(4) for y in poss(5)]
 
 
 def draw_circle(ps, center, r):
@@ -33,13 +42,16 @@ def draw_circle(ps, center, r):
 avg = lambda *xs: sum(xs) / len(xs)
 
 
-def make(im: Image) -> FPDF:
-    w, h = im.size
-    assert w == h, "should be square"
+# layers[rows[]]
+def make(layers: Iterable[Iterable[Tuple[Image.Image, bool]]]) -> FPDF:
     pdf = FPDF(unit="in", format="letter")
     pdf.add_page()
-    outer_radius = (outer_button / 2) - (inner_button / 2)
-    for x, y in button_positions:
-        pdf.image(im, x + outer_radius, y + outer_radius, inner_button, inner_button)
+    for layer in layers:
+        imgs = itertools.cycle(layer)
+        for x, y, inner_dims, outer_dims in button_positions:
+            im, full = next(imgs)
+            dims = outer_dims if full else inner_dims
+            pdf.image(im, *dims)
+    for x, y, *_ in button_positions:
         pdf.circle(x, y, outer_button)
     return pdf
--- api/make/index.py
+++ api/make/index.py
@@ -2,16 +2,48 @@ from . import buttonsheet
 from flask import Flask, request
 from PIL import Image
 import urllib.parse
+import itertools
+from werkzeug.exceptions import BadRequest
 
 app = Flask("sheetmaker")
 
 
+def open_image(buf):
+    try:
+        return Image.open(buf)
+    except (Image.UnidentifiedImageError, Image.DecompressionBombError):
+        raise BadRequest("bad image")
+
+
+def extract_rows():
+    markers = iter(request.form.getlist("marker"))
+    layers = zip(
+        map(open_image, request.files.getlist("layer-image")),
+        map(bool, request.form.getlist("layer-full")),
+    )
+    while True:
+        row = list(extract_layers(markers, layers))
+        if row:
+            yield row
+        else:
+            break
+
+
+def extract_layers(markers, layers):
+    for marker in markers:
+        if marker == "endrow":
+            break
+        yield next(layers)
+
+
 @app.route("/<path:path>", methods=["POST"])
 def make(path):
-    f = request.files["image"]
-    im = Image.open(f)
-    pdf = buttonsheet.make(im)
-    part = f.filename.rpartition(".")
+    fname = request.files["layer-image"].filename
+    rows = list(extract_rows())
+    import pprint
+    pprint.pprint(rows)
+    pdf = buttonsheet.make(rows)
+    part = fname.rpartition(".")
     fname = (part[0] or part[2]) + ".pdf"
     return pdf.output(), {
         "Content-Type": "application/pdf",
